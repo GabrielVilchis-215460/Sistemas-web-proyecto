ce5cb0924fa95464e89a6581e028afb30c976a59
Gabriel Vilchis <159979625+GabrielVilchis-215460@users.noreply.github.com>
2025-05-12 20:57:18 -0600

    fix geolocalization functions

--- js/script.js
+++ js/script.js
@@ -17,43 +17,45 @@ function mostrarCarga(event) {
 su ubicacion*/
 
 function GeoLocalization(){
-    var output = document.getElementById('mapa');
+    //var output = document.getElementById('mapa');
     if (navigator.geolocation){
-        output.innerHTML="<p>Tu dispositivo o navegador soporta la geolocalizacion!.";
+        alert("Tu dispositivo o navegador soporta la geolocalizacion!.");
         navigator.geolocation.getCurrentPosition(showPosition, showError);
     } else {
-        output.innerHTML="<p>Tu dispositivo o navegador no soporta la geolocalizacion!</p>";
+        alert("Tu dispositivo o navegador no soporta la geolocalizacion!");
     }
 }
 
 // funcion para obtener la ubicacion del usuario
-function showPosition(position){
-    document.getElementById("info").innerHTML = `
-        <ul>
-            <li>Latitud: 31.4924296</li>
-            <li>Longitud: -106.4147991</li>
-        </ul>
-    `;
-
+/*
+<ul>
+    li>Latitud: 31.4924296</li>
+    <li>Longitud: -106.4147991</li>
+</ul>*/
+  
+function showPosition(position) {
+    alert(`Tu ubicación es:\nLatitud: ${position.coords.latitude}\nLongitud: ${position.coords.longitude}`);
 }
 
+
 //funcion para mostrar errores
-function showError(error){
-    const d = document.getElementById("info");
+function showError(error) {
+    let mensaje = "";
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            d.innerHTML+="<p>El usuario ha denegado el permiso a la localizacion.</p>";
+            mensaje = "El usuario ha denegado el permiso a la localización.";
             break;
         case error.POSITION_UNAVAILABLE:
-            d.innerHTML+="<p>La informacion de la localizacion no esta disponible.</p>";
+            mensaje = "La información de la localización no está disponible.";
             break;
         case error.TIMEOUT:
-            d.innerHTML+="<p>El tiempo de espera para buscar la localizacion ha espirado.</p>";
+            mensaje = "El tiempo de espera para buscar la localización ha expirado.";
             break;
         case error.UNKNOWN_ERROR:
-            d.innerHTML+="<p>Ha ocurrido un error desconocido.</p>";
+            mensaje = "Ha ocurrido un error desconocido.";
             break;
     }
+    alert(mensaje);
 }
 
 // seccion de canvas
